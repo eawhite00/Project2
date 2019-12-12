5cb01701b738445e002b9297a57051b6bf619e0b
Jeff Huang <jeffshuang2@gmail.com>
2019-12-12 15:20:44 -0800

    Created api routes for creating a user, getting 10 random songs from the songs table, and grabbing current favorite songs that the user has liked.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,62 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/favsong", function(req, res) {
-    db.favoriteSongs
-      .create({
-        songName: req.body.name,
-        artistName: req.body.artist,
-        songDetails: req.body.details
+  // Get all favorite songs where the userId in the database matches the userId param. Include a join from the 'users' table so we have a grab on the foreign/primary keys.
+  app.get("/api/favorites/:userId", function(req, res) {
+    db.songs
+      .findAll({
+        where: {
+          userId: req.params.userId
+        },
+        include: [db.users]
       })
-      .then(function(response) {
-        console.log(response);
-        res.json({ id: response.insertId });
+      .then(function(dbFavsResult) {
+        res.json(dbFavsResult);
       });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
+  // Get 10 random songs from our database and list them out in JSON format. I'm thinking we can then format this data as an array and loop through it on the front-end and display them as questions to the user.
+  app.get("/api/random-songs", function(req, res) {
+    db.songs
+      .findAll({ order: Sequelize.literal("rand()"), limit: 10 })
+      .then(function(randomSongs) {
+        console.log(randomSongs);
+        res.json(randomSongs);
+      });
+  });
+
+  // Post method for creating a login and storing the information in our database.
+  // ** Still need to create the model for 'users'. **
+  app.post("/login", function(req, res) {
+    db.users
+      .create({
+        username: req.body.username,
+        password: req.body.password
+      })
+      .then(function(username) {
+        console.log(username);
+        res.json(username);
+      });
+  });
+
+  // Delete a favorite song by id
+  app.delete("/api/favorites/:id", function(req, res) {
+    db.songs.destroy({ where: { id: req.params.id } }).then(function(deletion) {
+      res.json(deletion);
     });
   });
 };
+
+// *NOT SURE IF THIS IS NEEDED **
+// app.post("/api/favsong", function(req, res) {
+//   db.songs
+//     .create({
+//       songName: req.body.name,
+//       artistName: req.body.artist,
+//       songDetails: req.body.details
+//     })
+//     .then(function(response) {
+//       console.log(response);
+//       res.json({ id: response.insertId });
+//     });
+// });
