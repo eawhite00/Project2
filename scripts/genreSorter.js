23643fdde102b45b5bc9419d25bc59657fb57c0e
e-holmes <bethholmes768@gmail.com>
2019-12-14 11:05:43 -0800

    Cleaned up my code to make it more flexible and prettier

--- scripts/genreSorter.js
+++ scripts/genreSorter.js
@@ -1,61 +1,40 @@
 // fetch user data
 var user = IMPORTUSERDATA;
 
-ratingStore(user.rapLike, user.rapDislike);
-ratingStore(user.countryLike, user.countryDislike);
-ratingStore(user.popLike, user.popDislike);
-ratingStore(user.rockLike, user.rockDislike);
-ratingStore(user.altLike, user.altDislike);
-
 var storageA;
-var storageB;
+var storageC;
 var percentage;
+var rapRate;
+var rockRate;
+var popRate;
+var altRate;
+var countryRate;
 
+ratingStore(user.rapLike, user.rapDislike, rapRate);
+rapRate = percentage;
+ratingStore(user.countryLike, user.countryDislike, countryRate);
+countryRate = percentage;
+ratingStore(user.popLike, user.popDislike, popRate);
+popRate = percentage;
+ratingStore(user.rockLike, user.rockDislike, rockRate);
+rockRate = percentage;
+ratingStore(user.altLike, user.altDislike, altRate);
+altRate = percentage;
+logRatings();
+
+// Code to store and process song ratings
 function ratingStore(like, dislike) {
-  // Rock Storage
+  //Store like count in slot A
   storageA = parseFloat(like);
-  storageB = parseFloat(dislike);
-  ratingMath();
-  rockRate = percentage;
-
-  // Country Storage
-  storageA = parseFloat(like);
-  storageB = parseFloat(dislike);
-  ratingMath();
-  countryRate = percentage;
-
-  // Rap Storage
-  storageA = parseFloat(like);
-  storageB = parseFloat(dislike);
-  ratingMath();
-  rapRate = percentage;
-
-  // Alternative Storage
-  storageA = parseFloat(like);
-  storageB = parseFloat(dislike);
-  ratingMath();
-  altRate = percentage;
-
-  // Pop Storage
-  storageA = parseFloat(like);
-  storageB = parseFloat(dislike);
-  ratingMath();
-  popRate = percentage;
-
-  // console log ratings
-  logRatings();
-}
-
-// Math to figure out percentages
-function ratingMath() {
-  var storageC = storageA + storageB;
-
-  // divide likes by total to get like percentage
+  //Store total song count in slot C
+  storageC = parseFloat(dislike) + storageA;
+  // Math to figure out percentages
   percentage = storageA / storageC;
   percentage = percentage * 100;
   percentage = Math.round(percentage);
 }
 
+// Logs score of genres
 function logRatings() {
   console.log("Rock like rating is: " + rockRate + "%");
   console.log("Country like rating is: " + countryRate + "%");
