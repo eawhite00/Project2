b875259c349675f678c8af18aca4ea399b7689be
Kensen <hirohtk@gmail.com>
2019-12-15 23:49:15 -0800

    refactored so that each like/dislike GETs a new song with either the random or recommended route.  still need to think about button positioning and the change module button bouncing when loading

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,6 @@ module.exports = function(app) {
     });
   });
 
-
   // Get one random song from our database.
   app.get("/api/random-song", function(req, res) {
     findRandomSong(null, function(randomSong) {
