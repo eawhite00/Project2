1cf947065f983d5c3b16a957bd0f0314e29f6d8e
Kensen <hirohtk@gmail.com>
2019-12-17 23:32:14 -0800

    attempting heroku deployment- took out the info in the .env file

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 //console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "2eecd783aacc4761818529ac1433a35e",
+  secret: "a73eb004d49344f199327df1da7dca08"
 };
