4960e2bbed372f95ff8206e4adcc757b6305f602
Kensen <hirohtk@gmail.com>
2019-12-18 19:25:42 -0800

    maybe heroku needs my database pw for some reason?  it really shouldn;t

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -96,25 +96,24 @@ module.exports = function(app) {
   // Get one random song from our database.
   app.get("/api/random-song", function(req, res) {
     findRandomSong(null, function(randomSong) {
-      // spotify.search({ type: "track", query: randomSong.songName }, function(
-      //   err,
-      //   data
-      // ) {
-      //   if (err) {
-      //     return console.log("Error occurred: " + err);
-      //   }
-      //   var spotifyPath = data.tracks.items;
-      //   console.log(spotifyPath[0].id);
-      //   var finalSong = {
-      //     song: randomSong.songName,
-      //     artist: randomSong.artist,
-      //     genre: randomSong.genre,
-      //     id: spotifyPath[0].id
-      //   };
-      //   //res.json(finalSong);
-      //   res.json(finalSong);
-      // });
-      res.json(randomSong);
+      spotify.search({ type: "track", query: randomSong.songName }, function(
+        err,
+        data
+      ) {
+        if (err) {
+          return console.log("Error occurred: " + err);
+        }
+        var spotifyPath = data.tracks.items;
+        console.log(spotifyPath[0].id);
+        var finalSong = {
+          song: randomSong.songName,
+          artist: randomSong.artist,
+          genre: randomSong.genre,
+          id: spotifyPath[0].id
+        };
+        //res.json(finalSong);
+        res.json(finalSong);
+      });
       // db.Song.findOne({ order: "rand()" }).then(function(randomSong) {
       //   console.log(randomSong);
       //   res.json(randomSong);
@@ -142,7 +141,24 @@ module.exports = function(app) {
     }).then(function(userIdResult) {
       var favoriteGenre = genrePrompt(userIdResult);
       findRandomSong(favoriteGenre, function(randomSong) {
-        res.json(randomSong);
+        spotify.search({ type: "track", query: randomSong.songName }, function(
+          err,
+          data
+        ) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+          var spotifyPath = data.tracks.items;
+          console.log(spotifyPath[0].id);
+          var finalSong = {
+            song: randomSong.songName,
+            artist: randomSong.artist,
+            genre: randomSong.genre,
+            id: spotifyPath[0].id
+          };
+          //res.json(finalSong);
+          res.json(finalSong);
+        });
       });
     });
   });
